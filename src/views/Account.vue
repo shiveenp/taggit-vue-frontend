4ac135b30c7ccc883aaa14b627c1303a30daf531
shiveenpandita <shiveenpandita@gmail.com>
2020-04-16 12:30:36 +1000

    feature/add-account-settings: fix some typos

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -10,7 +10,7 @@
             <div class="card">
                 <header class="card-header">
                     <p class="card-header-title">
-                        Persona Information
+                        Personal Information
                     </p>
                 </header>
                 <div class="card-content">
